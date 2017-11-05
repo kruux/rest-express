41123d90003c6f8527cce8d2f8db4435aec2343e
Eric Andersson <eric.andersson@rymdklubben.com>
2017-11-05 18:45:35 +0100

    Return array with data on GET request at /rest/launches. Remove node_modules folder and add it to .gitignore.

--- rest-express.js
+++ rest-express.js
@@ -1,9 +1,40 @@
 
 const express = require('express')
-const app = express()
+var cors = require('cors')
+
+var fs = require('fs')
 
 const axios = require('axios')
 
+const app = express()
+
+/* Download latest launches object */
+var launches
+var newLaunches
 axios.get('https://launchlibrary.net/1.2/launch/next/20').then(res => {
-	console.log(JSON.stringify(res.data.launches, null, "\t"))
+	launches = res.data.launches
+
+	/* Copy wanted data only to a new object array. */
+	newLaunches = launches.map(launch => {
+		newLaunch = Object.assign({},
+			{id: launch.id},
+			{rocket: {imageURL: launch.rocket.imageURL}},
+			{vidURLs: launch.vidURLs},
+			{name: launch.name},
+			{windowstart: launch.windowstart}
+		)
+		return newLaunch
+	})
+})
+
+/* Enable cors headers */
+app.use(cors())
+
+/* Listen for GET on /rest/launches, return the new json array */
+app.get('/rest/launches', (req, res) => {
+	res.json(newLaunches)
+})
+
+app.listen(3000, () => {
+	console.log('Server listening on port 3000')
 })
