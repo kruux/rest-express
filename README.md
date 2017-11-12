1410b7105ec3428c84174f446297b0d3bbd94c62
Eric Andersson <eric.andersson@rymdklubben.com>
2017-11-12 14:15:24 +0100

    Finish setInterval function and make small refactoring to the code to work better asynchronously. Add config.json file to store cfg data.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rest-express
 Simple rest api for rymdklubben.com
 
+Usage: edit config.json as desired, then run "npm test" without the quotationmarks.
--- rest-express.js
+++ rest-express.js
@@ -1,55 +1,82 @@
-
 const express = require('express')
 var cors = require('cors')
-
 var fs = require('fs')
-
 const axios = require('axios')
 
-const app = express()
+async function main(){
+	const app = express()
+	const cfgPath = "./config.json"
 
-/* Download latest launches object */
-var launches
-var newLaunches
-var x = 'Anka'
+	try{
+		var cfg = JSON.parse(fs.readFileSync(cfgPath))
+	}
+	catch(err){
+		console.log(err)
+		process.exit(1)
+	}
 
+	console.log(cfg)
 
-axios.get('https://launchlibrary.net/1.2/launch/next/20').then(res => {
-	launches = res.data.launches
+	var launches = await getLaunches()
+	if(!launches){
+		process.exit(1)
+	}
 
-	/* Copy wanted data only to a new object array. */
-	newLaunches = launches.map(launch => {
-		newLaunch = Object.assign({},
-			{id: launch.id},
-			{rocket: {imageURL: launch.rocket.imageURL}},
-			{vidURLs: launch.vidURLs},
-			{name: launch.name},
-			{windowstart: launch.windowstart}
-		)
-		return newLaunch
+	/* setInterval runs every 30 min */
+	setInterval(() => {
+		let result = getLaunches()
+		/* If not null, update the launches array which the json api returns */
+		if(result){
+			launches = result
+		}
+	},	1800000)
+
+	/* Enable cors headers */
+	app.use(cors())
+
+	/* Make sure connection closes after a request */
+	app.use((req, res, next) => {
+		res.setHeader('Connection', 'close')
+		return next()
 	})
-})
 
-/* setInterval runs function every 3000 ms */
-setInterval(() => {
-	if(x == 'Anka'){
-		x = 'Kalle'
+	/* Listen for GET on /rest/launches, return the new json array */
+	app.get('/rest/launches', (req, res) => {
+		res.json(launches)
+	})
+
+	app.listen(cfg.port, () => {
+		console.log('Server listening on port ' + cfg.port)
+	})
+}
+
+/* Download latest launches array */
+async function getLaunches(){
+	console.log('Fetching new launches array')
+	let launches
+	try{
+		launches = (await axios.get('https://launchlibrary.net/1.2/launch/next/20')).data.launches
+
+		/* Copy wanted data only to a new object array. */
+		var newLaunches = launches.map(launch => {
+			var newLaunch = Object.assign({},
+				{id: launch.id},
+				{rocket: {imageURL: launch.rocket.imageURL}},
+				{vidURLs: launch.vidURLs},
+				{name: launch.name},
+				{windowstart: launch.windowstart}
+			)
+			return newLaunch
+		})
 	}
-	else {
-		x = 'Anka'
+	catch(err){
+		console.log('getLaunches() failed, error: ' + err)
+		newLaunches = null
 	}
-	console.log(x)
-},	3000)
 
+	return newLaunches
+}
 
-/* Enable cors headers */
-app.use(cors())
-
-/* Listen for GET on /rest/launches, return the new json array */
-app.get('/rest/launches', (req, res) => {
-	res.json(newLaunches)
-})
-
-app.listen(3000, () => {
-	console.log('Server listening on port 3000')
-})
+if(require.main === module){
+	main()
+}
